b9b6cde75b2288d645a3d3425c0f3545d5a0d36a
Qingshan <dongqs@sunftech.cn>
2017-04-21 18:06:32 +0800

    descriptions

--- src/App.vue
+++ src/App.vue
@@ -4,11 +4,12 @@
     源代码<a href="https://github.com/dongqs/xiaokui">https://github.com/dongqs/xiaokui</a>
     <ul>
       <li>0.如认为题目过于简单可不做 直接联系我们</li>
-      <li>1.请每隔10分钟commit一次 这很重要</li>
+      <li>1.请每隔10分钟git-commit一次 这很重要</li>
       <li>2.按照设计图完成前端</li>
       <li>3.建议使用bootstrap或类似前端框架</li>
       <li>4.分component</li>
       <li>5.实现前端搜索</li>
+      <li>6.完成后放到github或类似平台上</li>
     </ul>
 
     <h3>这是设计图</h3>
